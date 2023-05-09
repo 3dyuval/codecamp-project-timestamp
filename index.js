a6404aed1e90ccf17c17983bf0c38849f0565e90
3dyuval <3dyuval@gmail.com>
2023-05-09 10:31:44 +0300

    pass all tests

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
-// index.js
-// where your node app starts
+const dotenv = require('dotenv')
+dotenv.config()
 
-// init project
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.status(200)
+  res.json({ greeting: 'hello Timestamp API' })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// A request to /api/:date? with a valid date should return a
+// JSON object with a utc key that is a string of the input date in the
+//  Thu, 01 Jan 1970 00:00:00 GMT
+//  format: Thu, 01 Jan 1970 00:00:00 GMT
 
+app.get('/api', function (req, res) {
+  res.status(200)
+  return res.send({ utc: new Date().toUTCString() })
+})
 
+app.get('/api/:date', function (req, res) {
+  const { date } = req.params
+
+  if (date === undefined) {
+    res.status(200)
+    return res.send({ utc: new Date().toUTCString() })
+  }
+
+  const timestamp = parseInt(date)
+  const utcFromTimestamp = new Date(timestamp)
+  const utcDate = new Date(date)
+
+  if (isNaN(timestamp) && !(isNaN(date) && utcDate instanceof Date)) {
+    res.status(200)
+    res.send({
+      utc: utcDate.toUTCString(),
+    })
+  }
+
+  if (!isNaN(timestamp) && utcFromTimestamp instanceof Date) {
+    res.status(200)
+    return res.send({
+      // unix: timestamp,
+      utc: utcFromTimestamp.toUTCString(),
+    })
+  }
+
+  res.status(400)
+  res.send({ error: 'Invalid Date' })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
